f30bd01fe0c085cb8d551bbb85d6dc88eb366eff
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2022-10-11 13:21:05 +0300

    2. Доменная модель проекта "АвтоМаг" [#504897]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,5 +1,16 @@
-CREATE TABLE IF NOT EXISTS cars
+CREATE TABLE IF NOT EXISTS auto_user
 (
     id   SERIAL PRIMARY KEY,
-    name TEXT
+    name TEXT,
+    login varchar(30) unique,
+    password text
+);
+
+CREATE TABLE IF NOT EXISTS auto_post
+(
+    id   SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    created timestamp,
+    auto_user_id int references auto_user(id)
 );
